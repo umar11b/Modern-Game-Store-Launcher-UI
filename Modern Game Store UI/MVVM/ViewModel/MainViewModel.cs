e47647e8f7006f99d3366f9e3c3adb9eddf4a1b5
uzama <uzama@DESKTOP-NR435N3>
2023-09-14 20:48:38 -0400

    Add project files.

---
+++ Modern Game Store UI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Modern_Game_Store_UI.Core;
+using Modern_Game_Store_UI.MVVM.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modern_Game_Store_UI.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged(); 
+            }
+
+            
+        }
+
+        public MainViewModel() 
+        { 
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+
+        }
+    }
+}
